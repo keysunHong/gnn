d06d70cc84cfbc71dfdec495241a95e04fbee321
孙伟红 <sunweihong@zichan360.com>
2020-07-06 22:43:27 +0800

    增加数据库连接

--- src/main/java/com/hxtx/server/MysqlTest.java
+++ src/main/java/com/hxtx/server/MysqlTest.java
@@ -1,5 +1,8 @@
 package com.hxtx.server;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * MysqlTest
  *
@@ -8,4 +11,13 @@ package com.hxtx.server;
  * @date: 2020/7/6 22:35
  **/
 public class MysqlTest {
+    public static void main(String[] args) {
+        List<Map<String, Object>> maps = JdbcUtils.executeQuery("select * from sc_login_user order by USER_ID DESC LIMIT 2");
+        for (Map<String, Object> map : maps) {
+            System.out.println("--------------------------------------------------");
+            for (String key : map.keySet()) {
+                System.out.println(key + " = " + map.get(key));
+            }
+        }
+    }
 }
