3d0f07ca557f920e53ba6416f141a1eeb988e374
孙伟红 <sunweihong@zichan360.com>
2020-07-06 22:43:16 +0800

    增加数据库连接

---
+++ src/main/java/com/hxtx/server/JdbcUtils.java
@@ -0,0 +1,11 @@
+package com.hxtx.server;
+
+/**
+ * JdbcUtils
+ *
+ * @desc:
+ * @author: sunweihong
+ * @date: 2020/7/6 22:32
+ **/
+public class JdbcUtils {
+}
---
+++ src/main/java/com/hxtx/server/MysqlTest.java
@@ -0,0 +1,11 @@
+package com.hxtx.server;
+
+/**
+ * MysqlTest
+ *
+ * @desc:
+ * @author: sunweihong
+ * @date: 2020/7/6 22:35
+ **/
+public class MysqlTest {
+}
